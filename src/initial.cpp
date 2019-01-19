bbf492d833649326002ca003e7976bfef7f9abaa
bindupriyatha <46836069+bindupriyatha@users.noreply.github.com>
2019-01-19 19:00:44 +0530

    Create initial.cpp

---
+++ src/initial.cpp
@@ -0,0 +1,7 @@
+#include "ros/ros.h"
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "TurtlesimPositionController_pubsub"); // connect to roscore
+	ros::NodeHandle n;                                     // node object
+}
